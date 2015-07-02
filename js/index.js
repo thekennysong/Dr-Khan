f8dcd1924328d36eb041f3195e6a687e834d550a
Kenny Song <kenny.song@multipleinc.com>
2015-07-02 14:31:56 -0500

    fun

---
+++ js/index.js
@@ -0,0 +1,8 @@
+ window.setTimeout(function(){
+              document.getElementById('holidays').style.visibility = 'hidden';
+          }, 0);
+ window.setTimeout(function(){
+              document.getElementById('holidays').style.visibility = 'visible';
+          }, 3900);
+document.getElementById('holidays').style.marginTop = '-100px';
+
